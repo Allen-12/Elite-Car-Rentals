c5e9769fb4beaab59464ddeac7929f62bb867d87
Stephm23 <Stephanie.mboya@strathmore.edu>
2020-05-09 22:31:09 +0300

    Home page and layouts

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.html"><img src = "images/elitelogo2.png" style="height: 100px; width: 130px;" ></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+          
+          
+          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+        <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="login.html" class="nav-link">Log In</a></li>
+        <li class="nav-item"><a href="signin.html" class="nav-link">Sign In</a></li>
+        
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!-- END nav -->
