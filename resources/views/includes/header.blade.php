0ef756278ea3ddbc69b8f613892aeb69c894c204
irungudeveloper <36546375+irungudeveloper@users.noreply.github.com>
2020-05-17 16:34:34 +0300

    Trying CSS

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,10 +6,12 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}"> -->
+    <link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="{{asset('css/animate.css')}}"> -->
+     <link rel="stylesheet" href="/css/animate.css">
 
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="/css/owl.carousel.min.css">
     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
 
