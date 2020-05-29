5d71ac90871186ad443e85d072cb8fb571e305e8
Allen-12 <allennjuguna@gmail.com>
2020-05-29 10:35:54 +0300

    Refactored controllers and routes

--- app/Http/Controllers/VehicleReserveController.php
+++ app/Http/Controllers/VehicleReserveController.php
@@ -8,14 +8,10 @@ use App\Http\Controllers\Controller;
 
 class VehicleReserveController extends Controller
 {
-
-
-
-public function index()
-{
-    $vehicles = DB::select('select * from car_description');
+    public function index()
+    {
+        $vehicles = DB::select('select * from car_description');
 //        dd($vehicles);
-    return view('reservation_process.vehicle_cards',compact('vehicles'));
+        return view('reservation_process.vehicle_cards', compact('vehicles'));
+    }
 }
-
-?>
